666ab205cccfbcf846491aa037306d2e028e876f
Miesieduo Veria <miesieduoveria@gmail.com>
2025-02-27 20:18:23 +0100

    Add

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -1,77 +1,90 @@
 "use client";
 import { useState, useEffect, useCallback } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import Link from 'next/link';
 import { useLanguage } from "@/app/context/LanguageContext";
 
 const translations = {
   en: [
     {
+      id: 1,
       title: "Exciting Logo Change Ahead",
       text: "Our club is set to unveil a new logo next week! Stay tuned for the reveal and share your thoughts.",
-      buttonText: "Learn More",
+      buttonText: "Learn More"
     },
     {
+      id: 2,
       title: "Players Gear Up for Next Match",
       text: "Our team is in full training mode as they prepare for the upcoming match against our rivals this Saturday.",
-      buttonText: "Learn More",
+      buttonText: "Learn More"
     },
     {
+      id: 3,
       title: "Youth Academy Shines",
       text: "Our youth academy has produced another star! Join us in celebrating the achievements of our rising talent.",
-      buttonText: "Learn More",
-    },
+      buttonText: "Learn More"
+    }
   ],
   ru: [
     {
+      id: 1,
       title: "Скоро новое изменение логотипа",
       text: "Наш клуб готовится представить новый логотип на следующей неделе! Оставайтесь с нами для раскрытия и делитесь своими мыслями.",
-      buttonText: "Узнать больше",
+      buttonText: "Узнать больше"
     },
     {
+      id: 2,
       title: "Игроки готовятся к следующему матчу",
       text: "Наша команда полна сил и готовится к предстоящему матчу против соперников в эту субботу.",
-      buttonText: "Узнать больше",
+      buttonText: "Узнать больше"
     },
     {
+      id: 3,
       title: "Успехи молодежной академии",
       text: "Наша молодежная академия вновь произвела звезду! Присоединяйтесь к нам, чтобы отпраздновать достижения нашего подрастающего таланта.",
-      buttonText: "Узнать больше",
-    },
+      buttonText: "Узнать больше"
+    }
   ],
   fr: [
     {
+      id: 1,
       title: "Changement de logo passionnant à venir",
       text: "Notre club s'apprête à dévoiler un nouveau logo la semaine prochaine ! Restez à l'écoute pour la révélation et partagez vos pensées.",
-      buttonText: "En savoir plus",
+      buttonText: "En savoir plus"
     },
     {
+      id: 2,
       title: "Les joueurs se préparent pour le prochain match",
       text: "Notre équipe est en pleine préparation pour le match à venir contre nos rivaux ce samedi.",
-      buttonText: "En savoir plus",
+      buttonText: "En savoir plus"
     },
     {
+      id: 3,
       title: "L'académie des jeunes brille",
       text: "Notre académie de jeunes a produit une nouvelle star ! Joignez-vous à nous pour célébrer les réalisations de notre talent en pleine émergence.",
-      buttonText: "En savoir plus",
-    },
+      buttonText: "En savoir plus"
+    }
   ],
   es: [
     {
+      id: 1,
       title: "Cambio de logo emocionante por delante",
       text: "¡Nuestro club está a punto de revelar un nuevo logo la próxima semana! Mantente atento para la revelación y comparte tus pensamientos.",
-      buttonText: "Aprender más",
+      buttonText: "Aprender más"
     },
     {
+      id: 2,
       title: "Los jugadores se preparan para el próximo partido",
       text: "Nuestro equipo está en plena fase de entrenamiento mientras se prepara para el próximo partido contra nuestros rivales este sábado.",
-      buttonText: "Aprender más",
+      buttonText: "Aprender más"
     },
     {
+      id: 3,
       title: "La academia juvenil brilla",
       text: "¡Nuestra academia juvenil ha producido otra estrella! Únete a nosotros para celebrar los logros de nuestro talento emergente.",
-      buttonText: "Aprender más",
-    },
-  ],
+      buttonText: "Aprender más"
+    }
+  ]
 };
 
 const Hero = () => {
@@ -135,34 +148,38 @@ const Hero = () => {
                 {slides[currentSlide].text}
               </motion.p>
 
-              <motion.div
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                className="text-white font-bold text-lg md:text-xl cursor-pointer"
+               <motion.div
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            className="text-white font-bold text-lg md:text-xl cursor-pointer"
+          >
+            <Link 
+              href={`/news?id=${slides[currentSlide].id}`}
+              className="relative flex items-center justify-evenly w-44 h-10 uppercase tracking-wide border-none bg-transparent transition-all duration-200 ease-[cubic-bezier(0.19,1,0.22,1)] opacity-60 hover:opacity-100 hover:tracking-wider group"
+            >
+              {slides[currentSlide].buttonText}
+              <span className="absolute top-0 left-1 h-full w-0 border-b-2 border-dashed border-yellow-300 opacity-70 transition-all duration-200 group-hover:w-[90%]"></span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="white"
+                viewBox="0 0 24 24"
+                height="15px"
+                width="15px"
+                className="transition-transform duration-200 group-hover:translate-x-2 animate-bounce"
               >
-                <button className="relative flex items-center justify-evenly w-44 h-10 uppercase tracking-wide border-none bg-transparent transition-all duration-200 ease-[cubic-bezier(0.19,1,0.22,1)] opacity-60 hover:opacity-100 hover:tracking-wider group">
-                {slides[currentSlide].buttonText}
-      <span className="absolute top-0 left-1 h-full w-0 border-b-2 border-dashed border-yellow-300 opacity-70 transition-all duration-200 group-hover:w-[90%]"></span>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="white"
-        viewBox="0 0 24 24"
-        height="15px"
-        width="15px"
-        className="transition-transform  duration-200 group-hover:translate-x-2 animate-bounce"
-      >
-        <path
-          strokeLinejoin="round"
-          strokeLinecap="round"
-          strokeMiterlimit="10"
-          strokeWidth="1.5"
-          className="bg-white"
-          stroke="#292D32"
-          d="M8.91016 19.9201L15.4302 13.4001C16.2002 12.6301 16.2002 11.3701 15.4302 10.6001L8.91016 4.08008"
-        ></path>
-      </svg>
-    </button>
-              </motion.div>
+                <path
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeMiterlimit="10"
+                  strokeWidth="1.5"
+                  className="bg-white"
+                  stroke="#292D32"
+                  d="M8.91016 19.9201L15.4302 13.4001C16.2002 12.6301 16.2002 11.3701 15.4302 10.6001L8.91016 4.08008"
+                ></path>
+              </svg>
+            </Link>
+               </motion.div>
+
             </motion.div>
 
             <div className="absolute bottom-44 flex space-x-2 z-10">
